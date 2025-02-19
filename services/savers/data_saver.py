366e7f4a6a68db81670cc79d35cc0a80c07288ef
Александр Королёв <forzaazzuri13@gmail.com>
2025-02-19 23:00:41 +0300

    + New features

--- services/savers/data_saver.py
+++ services/savers/data_saver.py
@@ -3,9 +3,10 @@ from abc import ABC, abstractmethod
 
 class DataSaver(ABC):
     @abstractmethod
-    def save(self, content: str, destination: str):
+    def save(self, channel_id: str, content: str, destination: str):
         """
         Abstract method для сохранения данных.
+        :param channel_id: ID канала.
         :param content: Текст, который нужно сохранить.
         :param destination: Местоназначение (путь к файлу или строка подключения к БД).
         """
--- services/text_processor.py
+++ services/text_processor.py
@@ -1,26 +1,95 @@
-from config.settings import GPT_MAX_TOKENS
+import math
+import os
+
+import tiktoken
+
+from config.settings import GPT_MAX_TOKENS, GPT_MODEL
 
 
 class TextProcessor:
-    @staticmethod
-    def split(text: str, max_tokens: int = GPT_MAX_TOKENS) -> list[str]:
+    def __init__(self, model: str = GPT_MODEL):
+        """
+        Инициализация TextProcessor с заданной GPT моделью.
+        :param model: Название GPT модели.
+        """
+        self.__encoding = tiktoken.encoding_for_model(model)
+        self.file_path = 'output/annotations.txt'
+
+    def split(self, text: str) -> list[str]:
         """
         Разделяет текст на части, каждая из которых не превышает max_tokens.
-        :param text: Текст субтитров.
+        :param text: Исходный текст.
         :param max_tokens: Максимальное количество токенов для одной части.
         :return: Список частей.
         """
+        tokens = self.__encode(text)
+        token_count = self.__token_count(text)
+
+        if token_count <= GPT_MAX_TOKENS:
+            return [text]
+
+        pieces = self.__pieces_count(token_count)
+
         chunks = []
-        current_chunk = []
+        for i in range(pieces):
+            start_idx = i * GPT_MAX_TOKENS
+            end_idx = (i + 1) * GPT_MAX_TOKENS
 
-        for word in text.split():
-            if len(' '.join(current_chunk)) + len(word) + 1 > max_tokens:
-                chunks.append(' '.join(current_chunk))
-                current_chunk = [word]
-            else:
-                current_chunk.append(word)
-
-        if current_chunk:
-            chunks.append(' '.join(current_chunk))
+            chunks.append(self.__decode(tokens[start_idx:end_idx]))
 
         return chunks
+
+    def save_to_file(self, content: str, mode: str = 'a', newline: str = '\n'):
+        """
+        Сохраняет содержимое в текстовый файл.
+        :param content: Содержимое, которое нужно сохранить в файл.
+        :param mode: Режим записи в файл ('a' - добавление, 'w' - перезапись).
+        :param newline: Символ или строка, которая будет использоваться для разделения записей.
+        """
+        try:
+            if not os.path.exists(self.file_path):
+                raise FileNotFoundError(f"Файл {self.file_path} не существует.")
+
+            if not os.access(self.file_path, os.W_OK):
+                raise PermissionError(f"Нет прав на запись в файл {self.file_path}.")
+
+            with open(self.file_path, mode=mode) as f:
+                f.write(content + newline)
+
+        except (OSError, PermissionError, FileNotFoundError) as e:
+            print(f"Ошибка при работе с файлом: {e}")
+
+    def __encode(self, text: str) -> list[int]:
+        """
+        Кодирует текст в список токенов.
+        :param text: Текст для кодирования.
+        :return: Список токенов.
+        """
+        return self.__encoding.encode(text)
+
+    def __decode(self, tokens: list[int]) -> str:
+        """
+        Декодирует список токенов в текст.
+        :param tokens: Список токенов для декодирования.
+        :return: Декодированный текст.
+        """
+        return self.__encoding.decode(tokens)
+
+    @staticmethod
+    def __pieces_count(token_count: int) -> int:
+        """
+        Подсчитывает количество частей, на которые можно разбить текст,
+        исходя из максимального количества токенов приходящихся на одну часть.
+        :param token_count: Общее количество токенов в тексте.
+        :return: Количество частей.
+        """
+        return math.ceil(token_count / GPT_MAX_TOKENS)
+
+    def __token_count(self, text: str) -> int:
+        """
+        Подсчитывает количество GPT токенов в тексте.
+        :param text: Анализируемый текст.
+        :return: Количество токенов в тексте.
+        """
+        tokens = self.__encode(text)
+        return len(tokens)
